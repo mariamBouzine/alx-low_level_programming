1ed6a1bbcc19c2a2213bd9024b57b9c5c095b2d2
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-15 16:39:07 -0700

    add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - function is the entry point 
  *
  * return 0 to indicate successful
  */
-int main(void) 
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
