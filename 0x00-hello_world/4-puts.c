03f7d16f054f8ae2cf8caa9ae543be89889f0064
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-15 16:49:21 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - the entry point 
+ * main - Entry point 
  *
- * return: 0 successful
+ * Return: Always 0 (successful) 
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
