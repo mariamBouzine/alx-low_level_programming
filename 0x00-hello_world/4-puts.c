e9c93449e3c5130e011e4348e3d2c1e54110d0da
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-15 16:32:50 -0700

    add task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * return 0 to indicate successful
  */
 int main(void) {
-    puts("/"Programming is like building a multilingual puzzle");
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
