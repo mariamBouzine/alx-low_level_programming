4b36a2e0add830f6c9280981da770e9207e0a9c3
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-16 18:27:33 -0700

    add task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,42 +7,42 @@
  */
 int main(void)
 {
-	int i, x, j, y, op1, op2;
-
-	i = x = j = y = 48;
-	while (y < 58)
+int i, x, j, y, op1, op2;
+	
+i = x = j = y = 48;
+while (y < 58)
+{
+	j = 48;
+	while (j < 58)
 	{
-		j = 48;
-		while (j < 58)
+		x = 48;
+		while (x < 58)
 		{
-			x = 48;
-			while (x < 58)
+			i = 48;
+			while (i < 58)
 			{
-				i = 48;
-				while (i < 58)
+				op1 = (y * 10) + j;
+				op2 = (x * 10) + i;
+				if (op1 < op2)
 				{
-					op1 = (y * 10) + j;
-					op2 = (x * 10) + i;
-					if (op1 < op2)
-					{
-						putchar(y);
-						putchar(j);
-						putchar(' ');
-						putchar(x);
-						putchar(i);
-						if (y == 57 && j == 56 && x == 57 && i == 57)
-							break;
-						putchar(',');
-						putchar(' ');
-					}
-					i++;
+					putchar(y);
+					putchar(j);
+					putchar(' ');
+					putchar(x);
+					putchar(i);
+					if (y == 57 && j == 56 && x == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
-				x++;
+				i++;
 			}
-			j++;
+			x++;
 		}
-		y++;
+		j++;
 	}
-	putchar('\n');
-	return (0);
+	y++;
+}
+putchar('\n');
+return (0);
 }
