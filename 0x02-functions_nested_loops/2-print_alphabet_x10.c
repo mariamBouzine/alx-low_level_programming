cf730709bd1c5fc07df15b36bd13ae3ce6cafeff
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-20 16:13:14 -0700

    add task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+    int i, letter;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 97; letter <= 122; letter++)
+        {
+            _putchar(letter);
+        }
+    }
+    _putchar('\n');
+}
