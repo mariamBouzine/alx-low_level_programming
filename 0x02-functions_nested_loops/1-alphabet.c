c816db0eef73b46a167acc008a70a10f28641eae
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-20 16:04:42 -0700

    add task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Prints alphabet to lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
