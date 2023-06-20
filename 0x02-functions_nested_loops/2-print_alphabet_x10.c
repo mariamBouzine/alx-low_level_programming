1a790146f23526460fc5d1a6297af5f944970402
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-20 16:45:27 -0700

    add task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i, letter;;
+	int i, letter;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (letter = 97; letter <= 122; letter++)
 		{
-			_putchar(letter;);
+			_putchar(letter);
 		}
 	_putchar('\n');
 	}
