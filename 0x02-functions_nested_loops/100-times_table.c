ac24780fa3267a95396de2f1fb73b505e627f009
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-06-20 18:09:58 -0700

    add task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
+ */
+void print_times_table(int n)
+{
+	int num, mult, res;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				res = num * mult;
+
+				if (res <= 99)
+					_putchar(' ');
+				if (res <= 9)
+					_putchar(' ');
+
+				if (res >= 100)
+				{
+					_putchar((res / 100) + '0');
+					_putchar(((res / 10)) % 10 + '0');
+				}
+				else if (res <= 99 && res >= 10)
+				{
+					_putchar((res / 10) + '0');
+				}
+				_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
