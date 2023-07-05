26740a409056ddf9af3aae3c49e52982f99a3df9
mariamBouzine <bouzine.mariam.solicode@gmail.com>
2023-07-04 18:56:32 -0700

    add task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
+	for (i = 0; i < n; i++)
+		*(dest + i) =  *(src + i);
 
 	return (dest);
 }
